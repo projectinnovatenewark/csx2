5abc3fd4b55151c789bc34152e9cde4757125865
palmerigarrett <palmerigarrett@gmail.com>
2020-12-08 15:11:19 -0500

    13_classes lesson

--- lessons/students/13_classes/13_classes.py
+++ lessons/students/13_classes/13_classes.py
@@ -2,16 +2,16 @@
 Understanding classes and scope within a class
 """
 
-# TITLE: Section 1- Classes Introduction (8 minutes)
+# TITLE: Section 1 - Classes Introduction (10 minutes)
 
 # Classes are like blueprints for creating objects in programming. Classes will have properties and
-# attributes that are privy only to objects instantiated in the given class. We ill go over
-# instatiating an instance of a class in a bit, but first, we need to define a class named "Cat".
+# attributes that are privy only to objects instantiated in the given class. We will go over
+# instatiating an instance of a class in a bit, but first we will define a class named "Cat".
 
 # TIP:
-# Unlike variables, Classes are defined with the first letter as a capital and the rest of the name
-# using camel case, whereas we like to use snake case to define variables.
-# This is "CamelCase"
+# Classes are defined with the first letter as a capital and the rest of the name using camel case,
+# whereas snake case is generally used to define variables and functions.
+# This is "camelCase"
 # This is "snake_case"
 # This is a "CamelCaseClass"
 
@@ -19,18 +19,19 @@ class Cat: # Here we are defining the class "Cat"
 
     kind = 'feline' # This is a "property". Properties are shared by all instances of a class.
 
-    # All classes have an "__init__" function.  __init__ is a special Python function that it is
+    # All classes have an "__init__" function.  __init__ is a special Python function that is
     # called automatically on an object creation statement. The computer science term for it is
     # constructor, as its job is to build an object of the type specified by the class.
     
     def __init__(self, name): # The __init__ funciton takes at least 2 parameters; self and 1 or
         self.name = name      # more attributes. The param, "self", refers to itself as an
                               # instantiated object. The param, "name", is an attribute of the "Cat"
-                              # class. You assign attributes using "self.var_name = var_name"
+                              # class. You assign attributes using "self.var_name = var_name" iwthin
+                              #   the scope of the "__init__" function.
 
 # Now we want to instantiate an instance of Cat. To do so, we need to assign each of the attributes
-# (other than "self") defined in the "__init__" function above. This can then be stored in a
-# variable in the format "var = ClassName("arg1, arg2")
+# (other than "self") defined in the "__init__" function above. The instantiation can then be stored
+# in a variable in the format "var = ClassName("arg1, arg2")
 felix = Cat('Felix') # The Cat class only takes one argument of name, so we instantiate the class
                      # with "Felix".
 boots = Cat('Boots') # The Cat class only takes one argument of name, so we instantiate the class
@@ -44,14 +45,16 @@ print(f"boots.kind: {boots.kind}") # shared by all Cats
 print(f"felix.name: {felix.name}") # unique to felix
 print(f"boots.name: {boots.name}") # unique to boots
 
+# TODO Section 1 of TODO 13 (7 minutes for students, 5 minute demo)
+
 ####################################################################################################
 
-# TITLE: Section 2- Methods aka Functions for Classes (4 minutes)
+# TITLE: Section 2 - Methods for Classes(5 minutes)
 
-# Methods are just functions that are defined within the scope of a class. Methods are alomst
-# exactly the same as functions, except methods are specific to the class it is defined in. Below
-# we are redefining the "Cat" class and defining a method that will append an activity to a given
-# cat's day.
+# Methods are functions that are defined within the scope of a class. Since methods are defined
+# within the scope of a class, they can only be called with an instantiated class object using dot
+# notation. Below we are redefining the "Cat" class and defining a method that will append an
+# activity to a given cat's day.
 
 class Cat:
 
@@ -83,19 +86,5 @@ print(f"Here is a list of {boots.name}'s activities for the day: \n {boots.activ
 
 # TIP: Remember "\n" will create a new line for anything after it in a print statement.
 
-# TODO Section 1 of TODO 13 (7 minutes for students, 5 minute demo)
 
-####################################################################################################
-
-# # TITLE: Section 3- Class Instantiation Review (5 minutes)
-​
-# Take the below class.
-class Person:
-    def __init__(self, fname, lname):
-        self.fname = fname
-        self.lname = lname
-
-    def identifier(self):
-        print(f"My name is {self.fname} {self.lname}")
-
-# TODO Section 2 of TODO 13 (5 minutes for students, 3 minute demo)
+# TODO Section 2 of TODO 13 (5 minutes for students, 4 minute demo)
